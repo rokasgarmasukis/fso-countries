79eae153cb7128f9d4f882b7ecfa690c1eae5855
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2022-03-25 06:02:58 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+import axios from "axios";
+
+const App = () => {
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+    .get("https://restcountries.com/v3.1/all")
+    .then(response => {
+      console.log(response.data);
+    })
+  },[search])
+
+  const handleSearchInput = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      find countries <input type="text" value={search} onChange={handleSearchInput} />
     </div>
   );
-}
+};
 
 export default App;
